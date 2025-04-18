1569452e3eb7cd987b470c68466dce861e7b32a1
emotionl1 <emotion_l@naver.com>
2025-04-19 03:51:43 +0900

    inde.tsx upload

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+// pages/index.tsx
+export default function Home() {
+    return (
+      <main className="min-h-screen">
+        <h1>레조넌스 정보 사이트</h1>
+        {/* 내용 추가 */}
+      </main>
+    )
+  }
