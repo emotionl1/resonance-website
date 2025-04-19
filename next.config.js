00b1a12b195ac45bc5a25f67a43ad907ec589de1
emotionl1 <emotion_l@naver.com>
2025-04-19 18:06:10 +0900

    pages/.. deleted and fix next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    swcMinify: true,
-    // App Router와 Pages Router를 함께 사용
-    experimental: {
-      appDir: true,
-    },
-    // 기본 출력 옵션 설정
     output: 'standalone',
   }
   
